6411edb78b43d045d8eb4789088a10cd7301430d
larsengel <larsengel@ymail.com>
2015-06-09 15:34:16 +0200

    Update save movement methods

--- SunriseProjekt/src/application/RobotMovements.java
+++ SunriseProjekt/src/application/RobotMovements.java
@@ -3,20 +3,21 @@ package application;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.lin;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
 
+import com.kuka.common.ThreadUtil;
 import com.kuka.roboticsAPI.conditionModel.ForceCondition;
+import com.kuka.roboticsAPI.executionModel.IFiredConditionInfo;
 import com.kuka.roboticsAPI.geometricModel.AbstractFrame;
 import com.kuka.roboticsAPI.geometricModel.Frame;
 import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.motionModel.IMotionContainer;
 
 public class RobotMovements {
 	
 	private Tool gripper;
 	private float break_force = 15;
-	private ForceCondition testForceCondition;
 
 	RobotMovements(Tool _gripper) {
 		gripper = _gripper;
-		testForceCondition = ForceCondition.createSpatialForceCondition(gripper.getDefaultMotionFrame(), break_force);
 	}
 	
 	/**
@@ -37,8 +38,15 @@ public class RobotMovements {
 	 * @param destination
 	 */
 	public void saveLinMove(AbstractFrame destination) {
-		gripper.getDefaultMotionFrame().move(lin(destination).breakWhen(testForceCondition).setJointVelocityRel(0.75));
-		
+		ForceCondition testForceCondition = ForceCondition.createSpatialForceCondition(gripper.getDefaultMotionFrame(), break_force);
+		IMotionContainer movement = gripper.getDefaultMotionFrame()
+				.move(lin(destination)
+						.breakWhen(testForceCondition)
+						.setJointVelocityRel(0.5));
+		IFiredConditionInfo firedCondInfo =	movement.getFiredBreakConditionInfo();
+		if (firedCondInfo != null) {
+			ThreadUtil.milliSleep(1000);
+		}
 	}
 	/**
 	 * PTP Move method, which stops when a specific force is reached
@@ -46,7 +54,14 @@ public class RobotMovements {
 	 * @param destination
 	 */
 	public void savePtpMove(AbstractFrame destination) {
-        gripper.getDefaultMotionFrame().move(ptp(destination).breakWhen(testForceCondition).setJointVelocityRel(0.75));
-		
+		ForceCondition testForceCondition = ForceCondition.createSpatialForceCondition(gripper.getDefaultMotionFrame(), break_force);
+		IMotionContainer movement = gripper.getDefaultMotionFrame()
+				.move(ptp(destination)
+						.breakWhen(testForceCondition)
+						.setJointVelocityRel(0.5));
+		IFiredConditionInfo firedCondInfo =	movement.getFiredBreakConditionInfo();
+		if (firedCondInfo != null) {
+			ThreadUtil.milliSleep(1000);
+		}
 	}
 }
